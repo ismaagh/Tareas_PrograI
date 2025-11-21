a978064bfefb0acdd31f6d46dc48b4321132a144
ismaagh <ismagh@LAPTOP-JM1I4CHV.localdomain>
2025-11-21 13:10:13 -0600

    Tarea 3 de Cuenta regresiva con Python

---
+++ Tareas_Segundo_Parcial/tarea3_cuenta_regresiva_for.py
@@ -0,0 +1,4 @@
+n = int(input("Ingresa el valor de n (debe ser entero positivo): "))
+
+for i in range(n,-1, -1): #n (inicio) , -1(criterio de parada), -1 (el decremento)
+	print(i)
---
+++ Tareas_Segundo_Parcial/tarea3_cuenta_regresiva_while.py
@@ -0,0 +1,7 @@
+n = int(input("Ingresa el valor de n (n debe ser  entero positivo > 0): "))
+
+i = n;
+while i >= 0:
+	print(i)
+	i -= 1
+
